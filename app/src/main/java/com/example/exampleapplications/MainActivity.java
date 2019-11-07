925198307d209c202098bf1ddec4acd603463f4c
Arpit Parmar <aparmar@techracers.io>
2019-11-07 15:19:16 +0530

    Created basic ListView for myFamily.

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.exampleapplications;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView  = findViewById(R.id.myListView);
+        ArrayList<String> myFamily = new ArrayList<>();
+
+        myFamily.add("Arpit");
+        myFamily.add("Aditya");
+        myFamily.add("Ajay");
+        myFamily.add("Sangita");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myFamily);
+        myListView.setAdapter(arrayAdapter);
     }
 }
