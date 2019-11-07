cd8fcd9f18207668e1457c9ea36916ec7703ea67
Arpit Parmar <aparmar@techracers.io>
2019-11-07 18:44:19 +0530

    Created a stopwatch with a reminding sound when timer completes.

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -2,39 +2,79 @@ package com.example.exampleapplications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
-import android.os.Handler;
-import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    TextView minutesTextView, secondsTextView;
+    Integer timeLeft=0;
+    CountDownTimer timer;
+
+    public void updateTime(int timeLeft) {
+        this.timeLeft = timeLeft;
+        this.setTimeTextViews();
+        this.seekBar.setProgress(timeLeft);
+    }
+
+    public void setTimeTextViews() {
+        this.minutesTextView.setText(String.format("%02d", timeLeft / 60));
+        this.secondsTextView.setText(String.format("%02d", timeLeft % 60));
+    }
+
+    public void goClicked(View view) {
+        if (timer != null)
+            timer.cancel();
+
+        timer = new CountDownTimer(timeLeft.longValue()*1000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if (timeLeft > 0)
+                    updateTime(timeLeft-1);
+            }
+
+            @Override
+            public void onFinish() {
+                MediaPlayer mplayer = MediaPlayer.create(getApplicationContext(), R.raw.air_horn);
+                mplayer.start();
+            }
+        };
+        timer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new CountDownTimer(10000, 1000) {
-            public void onTick(long millisecondsUntillDone) {
-                Log.i("Seconds Left!", String.valueOf(millisecondsUntillDone/1000));
-            }
+        seekBar = (SeekBar) findViewById(R.id.seekBar);
+        minutesTextView = (TextView) findViewById(R.id.minutesTextView);
+        secondsTextView = (TextView) findViewById(R.id.secondsTextView);
 
-            public void onFinish() {
-                Log.i("We're done!", "No more countdown!");
-            }
-        }.start();
+        seekBar.setMax(180);
 
-        /*
-        final Handler handler = new Handler();
-        Runnable run = new Runnable() {
+        this.setTimeTextViews();
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void run() {
-                Log.i("Hey yo!", "5 seconds passed by");
-                handler.postDelayed(this, 5000);
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateTime(progress);
             }
-        };
 
-        handler.post(run);
-        */
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                if (timer != null)
+                    timer.cancel();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
