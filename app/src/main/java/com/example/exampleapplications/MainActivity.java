e2b6ea0bcbb62922870248574c620f0e21de09fc
Arpit Parmar <aparmar@techracers.io>
2019-11-07 16:46:47 +0530

    Two types of timers: Runnable and CountDownTimer

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.exampleapplications;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisecondsUntillDone) {
+                Log.i("Seconds Left!", String.valueOf(millisecondsUntillDone/1000));
+            }
+
+            public void onFinish() {
+                Log.i("We're done!", "No more countdown!");
+            }
+        }.start();
+
+        /*
+        final Handler handler = new Handler();
+        Runnable run = new Runnable() {
+            @Override
+            public void run() {
+                Log.i("Hey yo!", "5 seconds passed by");
+                handler.postDelayed(this, 5000);
+            }
+        };
+
+        handler.post(run);
+        */
     }
 }
