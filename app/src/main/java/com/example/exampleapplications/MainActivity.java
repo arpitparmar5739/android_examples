319bfacc236c43c06fec55f3d9a68d868110519f
Arpit Parmar <aparmar@techracers.io>
2019-11-07 16:16:39 +0530

    Tables 1-20 application using ListView and Seekbar.

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.exampleapplications;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,23 +15,45 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+    ListView timesTableView;
+    ArrayAdapter<Integer> arrayAdapter;
+
+    public void updateTimesTableView(ArrayList<Integer> times) {
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, times);
+        timesTableView.setAdapter(arrayAdapter);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView friendsListView = findViewById(R.id.friendsListView);
+        final ArrayList<Integer> times = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10));
 
-        final ArrayList<String> myFriends = new ArrayList<>(Arrays.asList("Arpit", "Aditya", "Anuraj", "Aditi", "Eusuf"));
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, myFriends);
+        final SeekBar seekBar = (SeekBar) findViewById(R.id.seekBar);
+        seekBar.setMax(19);
 
-        friendsListView.setAdapter(arrayAdapter);
+        timesTableView = findViewById(R.id.timesTableView);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, times);
+        timesTableView.setAdapter(arrayAdapter);
 
-        friendsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "Hello "+myFriends.get(position), Toast.LENGTH_LONG).show();
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                for(int i=0; i<times.size(); i++){
+                    times.set(i, (i+1)*(progress+1));
+                }
+                updateTimesTableView(times);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
             }
         });
     }
