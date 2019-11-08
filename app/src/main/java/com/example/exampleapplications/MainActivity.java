ad6da45dca008b35b0a7c73c294fa8d8d02fea6c
Arpit Parmar <aparmar@techracers.io>
2019-11-08 19:53:05 +0530

    Initial layout for the Beauty and the Beast layout task.

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -3,12 +3,14 @@ package com.example.exampleapplications;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 }
