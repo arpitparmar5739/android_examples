ece39935486ad43a8772145fac7992405827c261
Arpit Parmar <aparmar@techracers.io>
2019-11-08 17:41:38 +0530

    Added TextView to show main weather and weather description.

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -2,13 +2,89 @@ package com.example.exampleapplications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    TextView weatherTextView;
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while(data != -1) {
+                    char currentChar = (char) data;
+                    result += currentChar;
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                String weatherInfo = jsonObject.getString("weather");
+
+                JSONArray arr = new JSONArray(weatherInfo);
+
+                for(int i=0; i<arr.length(); i++) {
+                    JSONObject jsonPart = arr.getJSONObject(i);
+
+                    Log.i("main", jsonPart.getString("main"));
+                    Log.i("description", jsonPart.getString("description"));
+                    weatherTextView.setVisibility(View.VISIBLE);
+                    weatherTextView.setText("Main: "+jsonPart.getString("main") + "\n" + "Description: "+jsonPart.getString("description"));
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void getWeather(View view) {
+        DownloadTask task = new DownloadTask();
+        task.execute("https://openweathermap.org/data/2.5/weather?q=" + editText.getText().toString() + "&appid=b6907d289e10d714a6e88b30761fae22");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        weatherTextView = findViewById(R.id.weatherTextView);
+        weatherTextView.setVisibility(View.INVISIBLE);
     }
 }
