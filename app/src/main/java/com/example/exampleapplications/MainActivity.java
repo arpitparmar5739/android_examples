71ac8021e685a32017cd75ce78a429de5ab0bc8f
Arpit Parmar <aparmar@techracers.io>
2019-11-15 12:41:18 +0530

    Implemented implicit intent calls for Web, Dial and Maps.

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.exampleapplications;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doSomething(View view) {
+        switch (view.getId()) {
+            case R.id.buttonBrowser:
+                Intent i1 = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(i1);
+                break;
+
+            case R.id.buttonOpenCall:
+                Intent i2 = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:8982428022"));
+                startActivity(i2);
+                break;
+
+            case R.id.buttonOpenMap:
+                Intent i3 = new Intent(Intent.ACTION_VIEW, Uri.parse("geo: 20.5937, 78.9629"));
+                startActivity(i3);
+                break;
+        }
     }
 }
