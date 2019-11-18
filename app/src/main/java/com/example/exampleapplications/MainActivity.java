e8218302840f786e9e53858378e754776b5f0105
Arpit Parmar <aparmar@techracers.io>
2019-11-18 15:22:48 +0530

    Implemented basic options menu.

--- app/src/main/java/com/example/exampleapplications/MainActivity.java
+++ app/src/main/java/com/example/exampleapplications/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.exampleapplications;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_menu, menu);
+//        return super.onCreateOptionsMenu(menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.m1:
+                Toast.makeText(this, "You touched Settings.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.m2:
+                Toast.makeText(this, "You touched  Mic.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.m3:
+                Toast.makeText(this, "You touched Edit.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
